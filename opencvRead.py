df8da6604992a67688289afef01fa2d00a6cb6b7
Arnau Ayerbe [Armag] <60605841+ArnauArmag@users.noreply.github.com>
2021-02-14 10:02:48 +0000

    No changes

--- opencvRead.py
+++ opencvRead.py
@@ -5,22 +5,33 @@ img=cv.imread('snake.jpeg')
 
 cv.imshow('Snake',img)
 '''
-capture=cv.VideoCapture(0)
-counter=0
-frames=[]
-while True:
-    counter=counter+1
-    isTrue, frame=capture.read()
-    print(counter)
-    #cv.imshow('Video',frame)
-    print(frame.shape)
-    #if cv.waitKey(20) & 0xFF==ord('d'):
-    frames.append(frame)
-    if cv.waitKey(20) & counter>2:
-        break
+def videoRecording():
+    capture=cv.VideoCapture(0)
+    counter=0
+    frames=[]
+    while True:
+        counter=counter+1
+        isTrue, frame=capture.read()
+        print(counter)
+        #cv.imshow('Video',frame)
+        print(frame.shape)
+        #if cv.waitKey(20) & 0xFF==ord('d'):
+        frames.append(frame)
+        if cv.waitKey(20) & counter>2:
+            break
+        
+    return frames
 
-for i in frames:
-    cv.imshow('Img',i)
-    cv.waitKey(0)
-capture.release()
-cv.destroyAllWindows()
+def showFrames(frames):
+    for i in frames:
+        cv.imshow('Img',i)
+        cv.waitKey(0)
+    #capture.release()
+    cv.destroyAllWindows()
+
+#Example
+newRecording=videoRecording()
+showFrames(newRecording)
+
+
+
